d00bcc8c440dcd020193950447dfd5bc42cd4200
EMP0247 <hutish@vebuin.com>
2023-06-13 18:04:34 +0530

    Final commit on 13/06/2023

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
-
-  resources :articles
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
 end
